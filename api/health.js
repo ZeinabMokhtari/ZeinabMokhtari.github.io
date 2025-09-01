088da266b3bd881d66eb86780337d9c865234b11
ZeinabMokhtari <144158273+ZeinabMokhtari@users.noreply.github.com>
2025-09-01 19:48:59 -0400

    Create health.js

---
+++ api/health.js
@@ -0,0 +1,5 @@
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') return res.status(204).end();
+  return res.status(200).send('ALIVE');
+};
